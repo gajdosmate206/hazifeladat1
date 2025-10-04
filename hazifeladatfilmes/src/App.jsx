1fe181ca5efcc2f5d717dd26eb53bc4600ad77de
Gajdos Máté <12345678+felhasznalonev@users.noreply.github.com>
2025-10-04 19:17:38 +0200

    javitottcommit

--- hazifeladatfilmes/src/App.jsx
+++ hazifeladatfilmes/src/App.jsx
@@ -1,8 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Fooldal from "./Fooldal";
+import Konyvkereso from "./Konyvkereso";
+import Filmkereso from "./Filmkereso";
+
 function App() {
 
   return (
     <>
-     
+     <div>
+      <nav style={{ marginBottom: "20px"}}>
+        <Link to="/">Nyitólap</Link>
+        <Link to="/konyvkereso">Könyv Kereső</Link>
+        <Link to="/filmkereso">Film Kereső</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Fooldal />} />
+        <Route path="/konyvkereso" element={<Konyvkereso />}/>
+        <Route path="/filmkereso" element={<Filmkereso />}/>
+      </Routes>
+     </div>
     </>
   )
 }
--- hazifeladatfilmes/src/main.jsx
+++ hazifeladatfilmes/src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 )
