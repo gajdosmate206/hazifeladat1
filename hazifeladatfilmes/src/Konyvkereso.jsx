925e57e50042276d4f6cd9627859fb27c9f7e2de
Gajdos Máté <12345678+felhasznalonev@users.noreply.github.com>
2025-10-04 18:43:55 +0200

    Konyvkeresocommit

---
+++ hazifeladatfilmes/src/Konyvkereso.jsx
@@ -0,0 +1,15 @@
+import Fooldal from "./Fooldal"
+
+function Konyvkereso() {
+const konyvcimek = ["Metro 2033", "Egri Csillagok", "Pál utcai fiúk", "Meinkampf", "Légy jó mindhalálig"]
+  return (
+    <>
+    <div>
+        <h2>Könyv kereső</h2>
+        <Fooldal items={konyvcimek} />
+    </div>
+    </>
+  )
+}
+
+export default Konyvkereso
