f3a4f613413081dc86e972d3c906ee19f8d0d371
Gajdos Máté <12345678+felhasznalonev@users.noreply.github.com>
2025-10-04 18:31:52 +0200

    kezdés

---
+++ hazifeladatfilmes/src/Fooldal.jsx
@@ -0,0 +1,13 @@
+function Fooldal() {
+
+  return (
+    <>
+     <div>
+      <h1>Üdv a kereső oldalunkon!</h1>
+      <p>Itt könyvekben és filmekben kereshetsz kedved szerint.</p>
+    </div>
+    </>
+  )
+}
+
+export default Fooldal
