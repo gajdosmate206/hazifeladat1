e23662df961310e7d574b362e6048ff6ab12bbdf
Gajdos Máté <12345678+felhasznalonev@users.noreply.github.com>
2025-10-04 19:03:49 +0200

    filmkeresocommit

---
+++ hazifeladatfilmes/src/Filmkereso.jsx
@@ -0,0 +1,13 @@
+function Filmkereso() {
+const filmnevek = ["Venom", "Star wars", "Nagyfiúk 2", "Másnaposok", "Fák jú, Tanár úr!"];
+  return (
+    <>
+     <div>
+      <h2>Film kereső</h2>
+      <Search kereses={filmnevek} />
+    </div>
+    </>
+  )
+}
+
+export default Filmkereso
