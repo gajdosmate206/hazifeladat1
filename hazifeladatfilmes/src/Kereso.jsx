0d579812aea33b743e9bc086fc156e69873bef27
Gajdos Máté <12345678+felhasznalonev@users.noreply.github.com>
2025-10-04 18:56:28 +0200

    keresocommit

--- hazifeladatfilmes/src/Kereso.jsx
+++ hazifeladatfilmes/src/Kereso.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
 
 function Kereso({ kereses }) {
-
+const [szuro, setSzuro] = useState("");
+const szurtKereses = szuro.kereses(keres => keres.toLowerCase().includes(filter.toLowerCase()));
   return (
     <>
-     
+     <input
+        type="text"
+        placeholder="Keresés..."
+        value={kereses}
+        onChange={(e) => setSzuro(e.target.value)}
+      />
+
+      <ul>
+        {szurtKereses.map((keres, i) => (
+          <li key={i}>{keres}</li>
+        ))}
+      </ul>
     </>
   )
 }
