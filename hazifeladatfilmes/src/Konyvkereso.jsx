68b415d80f991bc81d45f4a8e16b708c32a71155
Gajdos Máté <12345678+felhasznalonev@users.noreply.github.com>
2025-10-04 19:05:23 +0200

    konykeresocommitjavitott

--- hazifeladatfilmes/src/Konyvkereso.jsx
+++ hazifeladatfilmes/src/Konyvkereso.jsx
@@ -1,4 +1,4 @@
-import Fooldal from "./Fooldal"
+import Kereso from "./Kereso"
 
 function Konyvkereso() {
 const konyvcimek = ["Metro 2033", "Egri Csillagok", "Pál utcai fiúk", "Meinkampf", "Légy jó mindhalálig"]
